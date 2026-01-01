c5517642c28064a08f0246de10522f9f36dbde54
Cristian <cristian.ciulica@student.unitbv.ro>
2026-01-01 17:38:41 +0200

    ex1

--- Info_1.cpp
+++ Info_1.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
-#include <fstream>
 using namespace std;
-int main()
-{
-    cout << "hello";
+int a[100][100], i, j, s, n, m;
+int main(){
+    cin >> n >> m;
+    for (i = 0; i < n; i++)
+        for (j = 0; j < m; j++)
+            cin >> a[i][j];
+    for (i = 0; i < n; i++)
+        for (j = 0; j < m; j++)
+            if (a[i][j] % 2 == 0)
+                s += a[i][j];
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++)
+            cout << a[i][j] << " ";
+    cout << endl;
+    }
     return 0;
 }
